737d6e1bb7a067cfb20c8e7d3e02dea29cf8e417
anndjella <stankovicandjela53@gmail.com>
2025-08-28 10:23:53 +0200

    Add API integration to the frontend

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,5 +1,7 @@
 (ns app.core)
 
+(def backend-url "http://localhost:3000/api/predict")
+
 (defn q [sel] (.querySelector js/document sel))
 (defn qe [el sel] (.querySelector el sel))
 
@@ -9,6 +11,8 @@
     (when-not (js/isNaN n) (js/Math.floor n))))
 
 (defn to-binary ^number [^js el] (if (.-checked el) 1 0))
+
+(defn set-text! [sel s] (set! (.-textContent (q sel)) (or s "")))
 
 (defn form-input ^js [^js form]
   (let [el (fn [n] (qe form (str "[name='" n "']")))
@@ -23,15 +27,32 @@
 
 
 (defn form-valid? [^js form]
-  (.checkValidity form))
+  (.reportValidity form))
 
-(defn handle-submit! [^js e]
+(defn handle-submit [^js e]
   (.preventDefault e)
-  (let [form (.-currentTarget e)]
+  (let [form (.-currentTarget e)
+        input (form-input form)
+        json    (.stringify js/JSON input nil 2)] 
+    (js/console.log "Input (JSON):" json)
+    (set-text! "#errors" "")
+    (set-text! "#prediction" "")
     (if-not (form-valid? form)
-      (js/console.warn "Not valid form")
-      (js/console.log "Input: " (form-input form)))))
+      (set-text! "#errors" "Please fix the highlighted fields.")
+      (-> (js/fetch backend-url
+                    #js {:method "POST"
+                         :headers #js {"Content-Type" "application/json"}
+                         :body (.stringify js/JSON (form-input form))}) 
+          (.then (fn [resp]
+                   (if (.-ok resp) (.json resp)
+                       (js/Promise.reject (str "HTTP " (.-status resp))))))
+          (.then (fn [data]
+                   (let [m (js->clj data :keywordize-keys true)
+                         p (:prediction m)
+                         pstr (when p (.toFixed (js/Number p) 1))]
+                     (set-text! "#prediction" (if pstr (str "Predicted rating: " pstr) "No prediction")))))
+          (.catch (fn [err] (set-text! "#errors" (str err))))))))
 
 (defn ^:export init []
   (when-let [form (q "#predict-form")]
-    (.addEventListener form "submit" handle-submit!)))
+    (.addEventListener form "submit" handle-submit)))
